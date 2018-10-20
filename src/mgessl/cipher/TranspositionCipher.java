2f36892f0b81fd6ef0f3fd47cd0872c4f4fa5b50
mgessl <mgessl@student.tgm.ac.at>
2018-10-20 22:53:21 +0200

    Fertige Methoden

--- src/mgessl/cipher/TranspositionCipher.java
+++ src/mgessl/cipher/TranspositionCipher.java
@@ -64,44 +64,85 @@ public class TranspositionCipher implements Cipher{
 	}
 	
 	public String decrypt(String text) {
-		if (this.lvl<5 && this.lvl>1) {
 			text.toUpperCase();
+			
 			String one = "";
 			String two="";
 			String three="";
 			String four="";
+			String output="";
 			int countr=0;
 			int countr2=0;
-			for (int i=0; i<text.length();i++) {
-				if (text.charAt(i)== ' ') {
-					if (countr==0) {
-						one=text.substring(0, i);
-						countr++;
-						countr2=i+1;
-					} else if (countr==1) {
-						two=text.substring(countr2, i);
-						countr++;
-						countr2=i+1;
-					}else if (countr==2) {
-						three=text.substring(countr2, i);
-						countr++;
-						countr2=i+1;
-					}else if (countr==3) {
-						four=text.substring(countr2, text.length()-1);
+			int blanks=0;
+			for (int i=0;i<text.length();i++) {
+				if (text.charAt(i)==' ') blanks++;
+			}
+			if (blanks==1) {
+				for (int i=0; i<text.length();i++) {
+					if (text.charAt(i)==' '){
+						one= text.substring(0, i);
+						two=text.substring(i+1,text.length());
 					}
 				}
+				for (int i=0; i<one.length();i++) {
+					output=output+one.charAt(i);
+					if (i==two.length()) break;
+					output= output+two.charAt(i);
+				}
 			}
-			System.out.println(one);
-			System.out.println(two);
-			System.out.println(three);
-			System.out.println(four);
-
-			
-		}
-		return null;
+			if (blanks==2) {
+				int blank1=0;
+				int blank2=0;
+				for (int i=0; i<text.length();i++) {
+					if (text.charAt(i)==' ' && blank1==0) {
+						blank1=i;
+					} else if (text.charAt(i)==' ') {
+						blank2=i;
+					}	
+				}
+				one=text.substring(0,blank1);
+				two=text.substring(blank1+1, blank2);
+				three=text.substring(blank2+1, text.length());
+				for (int i=0; i<one.length();i++) {
+					output=output+one.charAt(i);
+					if (i==two.length()) break;
+					output= output+two.charAt(i);
+					if (i==three.length()) break;
+					output= output+three.charAt(i);
+				}
+			}
+			if (blanks==3) {
+				int blank1=0;
+				int blank2=0;
+				int blank3=0;
+				for (int i=0; i<text.length();i++) {
+					if (text.charAt(i)==' ' && blank1==0) {
+						blank1=i;
+					} else if (text.charAt(i)==' '&& blank2==0) {
+						blank2=i;
+					}	else if (text.charAt(i)==' ') {
+						blank3=i;
+					}	
+				}
+				one=text.substring(0,blank1);
+				two=text.substring(blank1+1, blank2);
+				three=text.substring(blank2+1, blank3);
+				four=text.substring(blank3+1, text.length());
+				for (int i=0; i<one.length();i++) {
+					output=output+one.charAt(i);
+					if (i==two.length()) break;
+					output= output+two.charAt(i);
+					if (i==three.length()) break;
+					output= output+three.charAt(i);
+					if (i==four.length()) break;
+					output= output+four.charAt(i);
+				}
+				
+			}
+		return output;
 	}
 	public static void main(String[] args) {
 		TranspositionCipher t= new TranspositionCipher(4);
-		System.out.println(t.decrypt("ENIKIHEWREDRATNAN IWRLCVRIRNEGREZU"));
+		System.out.println(t.decrypt("RSO AP MO MP"));
 	}
 }
